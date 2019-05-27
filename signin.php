a565b3794e29647e5e0a2c845a6134fd3531238a
egavilan1987 <enmanuelgc@gmail.com>
2019-05-27 15:15:15 -0400

    Update and rename register.php to signin.php

---
+++ signin.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Simple PHP Login and Register System</title>		
+	</head>  
+<body>
+	<ul>
+	  <a href="#">Sign in</a>&nbsp; 
+	  <a href="signup">Sign up</a>&nbsp;
+	</ul>
+
+	<form method='post' action='signin.php'>
+	<table width='400' border='3' align='center'>
+		<tr>
+			<th bgcolor='silver' colspan='5'>Sign in</h>
+		</tr>
+		<tr >
+			<td align='right'>User name:</td>
+			<td><input type='text' name='name' maxlength="50" required >
+			</td>
+		</tr>
+		<tr>
+			<td align='right'>Password:</td>
+			<td><input type='text' name='description' maxlength="500" required >
+			</td>
+		</tr>
+
+		<tr>
+			<td align='center' colspan='6'>
+			<input type='submit' name='submit' value='Submit'>
+			</td>
+		</tr>
+        <tr>
+			<td align='center' colspan='6'>
+			<a href="signup.php">Sign up</a>&nbsp;
+			</td>
+		</tr>
+	  </table>  
+	</form><br>
+</body>
+
+</html>
