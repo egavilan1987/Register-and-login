5d24e74f5d4abc49f764ea20602afc0bc08aa5c9
egavilan1987 <enmanuelgc@gmail.com>
2019-05-27 15:20:25 -0400

    Update init.php

--- init.php
+++ init.php
@@ -1,1 +1,32 @@
 
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Simple PHP Login and Register System</title>		
+	</head>  
+<body>
+
+	<ul align='right'>
+	  <a href="signup.php">Log out</a>&nbsp; 
+	</ul>
+	<h3>Welcome</h3>
+	<table width='400' border='3' align='center'>
+		<tr>
+			<th bgcolor='dodgerBlue' colspan='5'>Sign in</h>
+		</tr>
+		<tr >
+			<td align='right'>User name:</td>
+			<td><input type='text' name='name' maxlength="50" required >
+			</td>
+		</tr>
+		<tr>
+			<td align='right'>Password:</td>
+			<td><input type='text' name='description' maxlength="500" required >
+			</td>
+		</tr>
+
+	  </table>  
+
+</body>
+
+</html>
