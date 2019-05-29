e537a8a0fef3af1c948d9a878e58a499fd5f8373
egavilan1987 <enmanuelgc@gmail.com>
2019-05-29 15:58:59 -0400

    Update signin.php

--- signin.php
+++ signin.php
@@ -8,30 +8,29 @@
 	  <a href="#">Sign in</a>&nbsp; 
 	  <a href="signup">Sign up</a>&nbsp;
 	</ul>
-
+	<br><br>
 	<form method='post' action='signin.php'>
-	<table width='400' border='3' align='center'>
-		<tr>
-			<th bgcolor='dodgerBlue' colspan='5'>Sign in</h>
-		</tr>
-		<tr >
-			<td align='right'>User name:</td>
-			<td><input type='text' name='name' maxlength="50" required >
-			</td>
-		</tr>
-		<tr>
-			<td align='right'>Password:</td>
-			<td><input type='text' name='description' maxlength="500" required >
-			</td>
-		</tr>
-
-		<tr>
-			<td align='center' colspan='6'>
-			<input type='submit' name='submit' value='Submit'>
-			</td>
-		</tr>
-	  </table>  
-	</form><br>
+      <table width='400' border='3' align='center'>
+          <tr>
+              <th bgcolor='dodgerBlue' colspan='2'>Sign in</th>
+          </tr>
+          <tr >
+              <td align='right'>User name:</td>
+              <td><input type='text' name='name' maxlength="50" required>
+              </td>
+          </tr>
+          <tr>
+              <td align='right'>Password:</td>
+              <td><input type='text' name='description' maxlength="500" required>
+              </td>
+          </tr>
+          <tr>
+              <td align='center' colspan='2'>
+              <input type='submit' name='submit' value='Submit'>
+              </td>
+          </tr>
+      </table>  
+	</form>
 </body>
 
 </html>
