d8ea4733547c344a021085113a6487fa0b31a863
egavilan1987 <enmanuelgc@gmail.com>
2019-05-29 16:06:24 -0400

    Update init.php

--- init.php
+++ init.php
@@ -1,32 +1,64 @@
-
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>Simple PHP Login and Register System</title>		
 	</head>  
 <body>
-
-	<ul align='right'>
-	  <a href="signup.php">Log out</a>&nbsp; 
+	<ul>
+	  <a href="signin.php">Sign in</a>&nbsp; 
+	  <a href="#">Sign up</a>&nbsp;
 	</ul>
-	<h3>Welcome</h3>
-	<table width='400' border='3' align='center'>
-		<tr>
-			<th bgcolor='dodgerBlue' colspan='5'>Sign in</h>
-		</tr>
-		<tr >
-			<td align='right'>User name:</td>
-			<td><input type='text' name='name' maxlength="50" required >
-			</td>
-		</tr>
-		<tr>
-			<td align='right'>Password:</td>
-			<td><input type='text' name='description' maxlength="500" required >
-			</td>
-		</tr>
-
-	  </table>  
-
+	<br><br>
+	<form method='post' action='addProducts.php'>
+      <table width='500' border='3' align='center'>
+          <tr>
+              <th bgcolor='dodgerBlue' colspan='2'>Registration Form</th>
+          </tr>
+          <tr>
+              <td align='right'>First Name:</td>
+              <td><input type='text' name='firstname' maxlength="50" required >
+              </td>
+          </tr>
+          <tr>
+              <td align='right'>Last Name:</td>
+              <td><input type='text' name='lastname' maxlength="50" required >
+              </td>
+          </tr>
+          <tr>
+              <td align='right'>Email :</td>
+              <td><input type='email' name='email' maxlength="50" required>
+              </td>
+          </tr>
+          <tr>
+              <td align='right'>Username: </td>
+              <td><input type='username' name='username' maxlength="50" required>
+              </td>
+          </tr>
+          <tr>
+              <td align='right'>Password: </td>
+              <td><input type='password' name='password' maxlength="50" required>
+              </td>
+          </tr>
+          <tr>
+              <td align='right'>Gender: </td>
+              <td>
+                <input type="radio" name="gender" value="male" checked> Male
+                <input type="radio" name="gender" value="female"> Female
+                <input type="radio" name="gender" value="other"> Other  
+              </td>
+          </tr>
+          <tr>
+              <td align='right'>Birthday: </td>
+              <td><input type='date' name='birthday' required>
+              </td>
+          </tr>
+          <tr>
+              <td align='center' colspan='2'>
+              <input type='submit' name='submit' value='Submit'>
+              </td>
+          </tr>  
+       </table>  
+	</form>
 </body>
 
 </html>
